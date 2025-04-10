c51e92103938d8ad36f414b2ae5e21c1d204547d
M4nue <mrodcur12042005@gmail.com>
2025-04-10 08:25:36 +0200

    Cambios leves

--- 128particiones.sh
+++ 128particiones.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Verificar si el usuario tiene permisos de root
-#SCRIPT SECUNDARIO
+
 if [ "$EUID" -ne 0 ]; then
   echo "Este script debe ejecutarse como root, o tienes que tener permisos de administrador"
 else
